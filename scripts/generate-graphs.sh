46505d4beb3a539a1808a96ed9bfc2fd73d1c278
Benjamin De Kosnik <bkoz@gnu.org>
2026-05-17 14:19:30 -0700

    update to milestone 2

--- scripts/generate-graphs.sh
+++ scripts/generate-graphs.sh
@@ -1,3 +1,9 @@
-./a60-meta-collection.exe ../data Animation 0 "" 0
+#!/usr/bin/env bash
 
-./a60-meta-collection.exe ../data Animation 0 invincible 1
+./a60-meta-collection.exe ../data "Animation" "" "" 0
+
+./a60-meta-collection.exe ../data "Invincible S1-4" "" "invincible" 1
+./a60-meta-collection.exe ../data "Arcane" "" "arcane" 0
+
+./a60-meta-collection.country.exe ../data/dan-da-dan-210-week.json "Dan Da Dan 210"
+./a60-meta-collection.country.exe ../data/zootopia-2-week.json "Zootopia 2"
