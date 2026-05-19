77ae3723f3b00c0f1050f17549ea46e5a9361a44
Benjamin De Kosnik <bkoz@gnu.org>
2026-05-18 17:44:29 -0700

    update charts and tables

--- scripts/generate-graphs.sh
+++ scripts/generate-graphs.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 ./a60-meta-collection.exe ../data "Animation" "" "" 0
+./a60-meta-collection.exe ../data "Animation" 2026 "" 1
+./a60-meta-collection.exe ../data "Animation" 2025 "" 1
+./a60-meta-collection.exe ../data "Animation" 2024 "" 1
+./a60-meta-collection.exe ../data "Animation" 2023 "" 1
+./a60-meta-collection.exe ../data "Animation" 2022 "" 1
+./a60-meta-collection.exe ../data "Animation" 2021 "" 1
+./a60-meta-collection.exe ../data "Animation" 2020 "" 1
 
 ./a60-meta-collection.exe ../data "Invincible S1-4" "" "invincible" 1
 ./a60-meta-collection.exe ../data "Arcane" "" "arcane" 0
+./a60-meta-collection.exe ../data "Spider Man Universe" "" "spider" 0
 
 ./a60-meta-collection.country.exe ../data/dan-da-dan-210-week.json "Dan Da Dan 210"
 ./a60-meta-collection.country.exe ../data/zootopia-2-week.json "Zootopia 2"
+./a60-meta-collection.country.exe ../data/maul-shadow-lord-101-week.json "Star Wars Maul 101-104"
